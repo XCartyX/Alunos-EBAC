ff78f0d6c477b05605fca680b16b5681062aaab7
XCartyX <125204887+XCartyX@users.noreply.github.com>
2023-02-12 13:04:41 -0300

    opção de saída
    incluso no sistema a opção de saída para o usuário

--- AlunosEBAC.c
+++ AlunosEBAC.c
@@ -130,6 +130,7 @@ int main()
         printf("\t1 - Registrar nome\n");
 	    printf("\t2 - Consultar nome\n");
         printf("\t3 - Deletar nome\n\n");
+        printf("\t4 - Sair do sistema\n\n");
 	    printf("Opção:");//Fim do menu
 	
 	
@@ -149,6 +150,11 @@ int main()
 		
 		case 3:
 		deletar();
+		
+		case 4:
+		printf("Obrigado por utilizar o sistema!\n");
+		return 0;
+		break;
 		
 		default:
 		printf("opção nâo disponivel!\n");
